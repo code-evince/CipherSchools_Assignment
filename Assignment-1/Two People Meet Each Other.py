b935093e8b716746f898a6a743b25940f7d02e03
Bishnu Prasad Nayak <canaryedition@gmail.com>
2021-02-14 11:17:29 +0530

    Two People Meet Each Other

--- Assignment-1/Two People Meet Each Other.py
+++ Assignment-1/Two People Meet Each Other.py
@@ -1,1 +1,12 @@
+import math
+def Meet():
+    print("enter p1,v1,p2,v2 separeted by spaces :  ")
+    p1,v1,p2,v2 = map(int,input().split())
+    if ((p1>p2 and v1>=v2) or (p1<p2 and v1<=v2)):
+        return False
+    return (math.abs(p1-p2) % math.abs(v1-v2))==0
 
+if(Meet()):
+    print("YES")
+else:
+    print("NO")
