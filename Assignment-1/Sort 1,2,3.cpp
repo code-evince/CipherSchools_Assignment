f636b84e9fee7871bc9a5016a6418503c749822b
code-evine <canarediion@gmail.com>
2021-02-14 22:44:26 +0530

    Alternate Sort

---
+++ Assignment-1/Sort 1,2,3.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void dutchSort(int arr[], int n){
+    int lo=0,mid=0,hi=n-1;
+    while(mid<=hi){
+        switch(arr[mid]){
+
+            case 0: swap(arr[lo],arr[mid]);
+                    mid++,lo++;
+                    break;
+
+            case 1: mid++;
+                    break;
+
+            case 2: swap(arr[mid],arr[hi]);
+                    hi--;
+                    break;
+        }
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    dutchSort(arr,n);
+        for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
