96905db44006c4b6e0e7888b0d095efa7a1a2918
code-evince <canaryedition@gmail.com>
2021-02-19 20:58:23 +0530

    Update 2

--- Assignment-3/Intersection Point.cpp
+++ Assignment-3/Intersection Point.cpp
@@ -1,89 +1,96 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Node
-{
-public:
-    int data;
-    Node *next;
-    Node()
-    {
-        Node(0);
-    }
-
-    Node(int data)
-    {
-        this->data = data;
-        this->next = NULL;
-    }
+struct Node {
+	int data;
+	Node* next;
+	Node (int x){
+	    data=x;
+	    next=NULL;
+	}
 };
 
-void printList(Node *head)
+int getCount(Node* head)
 {
-    Node *ptr = head;
-    while (ptr != NULL)
-    {
-        cout << ptr->data << "->";
-        ptr = ptr->next;
+    Node* curr = head;
+    int count = 0;
+    while (curr != NULL) {
+        count++;
+        curr = curr->next;
     }
-    cout << "\n";
+    return count;
 }
 
-int getSize(Node *head)
+int _getIntersection(int d, Node* head1, Node* head2)
 {
-    Node *ptr = head;
-    int size = 0;
-    while (ptr != NULL)
-    {
-        size++;
-        ptr = ptr->next;
-    }
-    return size;
-}
+    Node* current1 = head1;
+    Node* current2 = head2;
 
-Node *findIntersectionHelper(Node *larger, Node *smaller, int difference)
-{
-    Node *ptr = larger, *qtr = smaller;
-    for (int i = 0; i < difference; i++)
-    {
-        if (ptr == NULL)
-            return NULL;
-        ptr = ptr->next;
-    }
-    while (ptr != NULL && qtr != NULL)
-    {
-        if (ptr == qtr)
-        {
-            return ptr;
+    for (int i = 0; i < d; i++) {
+        if (current1 == NULL) {
+            return -1;
         }
-        ptr = ptr->next;
-        qtr = qtr->next;
+        current1 = current1->next;
     }
-    return NULL;
+
+    while (current1 != NULL && current2 != NULL) {
+        if (current1 == current2)
+            return current1->data;
+
+        current1 = current1->next;
+        current2 = current2->next;
+    }
+
+    return -1;
 }
-Node *findIntersection(Node *a, Node *b)
+
+int getIntersection(Node* head1, Node* head2)
 {
-    if (a == NULL || b == NULL)
-    {
-        return NULL;
+    int c1 = getCount(head1);
+    int c2 = getCount(head2);
+    int d;
+
+    if (c1 > c2) {
+        d = c1 - c2;
+        return _getIntersection(d, head1, head2);
     }
-    int countA = getSize(a);
-    int countB = getSize(b);
-    int d = countA - countB;
-    if (d >= 0)
-    {
-        return findIntersectionHelper(a, b, d);
-    }
-    else
-    {
-        return findIntersectionHelper(b, a, -1 * d);
+    else {
+        d = c2 - c1;
+        return _getIntersection(d, head2, head1);
     }
 }
 
 int main()
 {
-  SinglyLinkedList sll;
-  for (int i = 5; i >= 1; i--)
-      sll.insertAtBeginning(i);
+	/*
+		Creation of two linked lists
 
+		1st 3->6->9->15->30
+		2nd 10->15->30
+
+		15 is the intersection point
+	*/
+
+	Node* newNode;
+
+	Node* head1 = new Node(10);
+
+	Node* head2 = new Node(3);
+
+	newNode = new Node(6);
+	head2->next = newNode;
+
+	newNode = new Node(9);
+	head2->next->next = newNode;
+
+	newNode = new Node(15);
+	head1->next = newNode;
+	head2->next->next->next = newNode;
+
+	newNode = new Node(30);
+	head1->next->next = newNode;
+
+	head1->next->next->next = NULL;
+
+	cout <<getIntersection(head1, head2);
 }
